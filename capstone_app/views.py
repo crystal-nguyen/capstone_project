b4e77a18a539916b1f1d89802cf87f74cb83f282
Crystal Nguyen <cnguye30@sundevils.asu.edu>
2021-04-22 20:29:11 -0700

    added a default test

--- capstone_app/views.py
+++ capstone_app/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .azure_models import *
-from os import *
 from capstone_project.settings import BASE_DIR
 from json import dumps
 import pandas as pd
 from io import StringIO
+from django_tables2.tables import Table
 
 # Create your views here.
 
@@ -19,20 +19,32 @@ def dashboard(request):
 
 
 def test(request):
+    default_data = pd.read_csv('./capstone_app/static/img/tabulardata.csv')
+    data_html = default_data.to_html()
     if request.method == "POST":
         # get file contents
         uploaded_image = request.FILES["image"]
         uploaded_csv = request.FILES["file"]
 
         models = AzureModels()
-        data = models.combinedModel(uploaded_csv, uploaded_image)
-        if(data == 'no_tumor'):
+        data_test = models.combinedModel(uploaded_csv, uploaded_image)
+        if(data_test == 'no_tumor'):
+            data_test = "Sample indicates non-cancerous"
+        else:
+            data_test = "Sample indicates cancerous"
+        return render(request, 'test.html', {"result_test": data_test, "loaded_data": data_html})
+
+    else:
+        tabular_file = open('./capstone_app/static/img/tabulardata.csv','rb')
+        image_file = open('./capstone_app/static/img/demo_image.png','rb')
+
+        models = AzureModels()
+        data = models.combinedModel(tabular_file, image_file)
+        if (data == 'no_tumor'):
             data = "Sample indicates non-cancerous"
         else:
             data = "Sample indicates cancerous"
-
-        return render(request, 'test.html', {"result": data})
-
+        return render(request, 'test.html', {"loaded_data": data_html, "result": data})
     # main page to test the model
     # this is where we will call the function from azure_models.py
-    return render(request, 'test.html')
+    return render(request, 'test.html', {"loaded_data": data_html})
